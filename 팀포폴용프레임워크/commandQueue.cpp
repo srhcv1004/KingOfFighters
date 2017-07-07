e1d6e968ce12ca48602458d17bc0ed2df01ecc0d
kys9096 <kys9096@naver.com>
2017-07-07 16:19:16 +0900

    commandQueue
    기본 원형. commandQueueDefine.h 헤더 include 바람.

---
+++ 팀포폴용프레임워크/commandQueue.cpp
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+#include "commandQueue.h"
+
+CommandQueue::CommandQueue(){}
+CommandQueue::~CommandQueue(){}
+
+HRESULT CommandQueue::init(void)
+{
+	initKeyBuffer();
+	initCommandBuffer();
+
+	_debugMode = true;
+
+	return S_OK;
+}
+
+void CommandQueue::release(void)
+{
+	updateKeyBuffer();
+	updateCommandBuffer();
+}
+
+void CommandQueue::update(void)
+{
+	updateKeyBuffer();
+	updateCommandBuffer();
+}
+
+void CommandQueue::render(void)
+{
+	if (_debugMode)
+	{
+		renderKeyBuffer();
+		renderCommandBuffer();
+	}
+}
+void CommandQueue::initKeyBuffer()
+{
+	for (int i = 0; i < KEYBUFFERSIZE; ++i)
+	{
+		_keyBuffer[i] = C_IDLE;
+	}
+}
+
+void CommandQueue::initCommandBuffer()
+{
+	for (int i = 0; i < COMMANDBUFFERSIZE; ++i)
+	{
+		_commandBuffer[i] = C_IDLE;
+	}
+}
+
+void CommandQueue::updateKeyBuffer()
+{
+	for (int i = KEYBUFFERSIZE - 2; i >= 0; --i)
+	{
+		_keyBuffer[i + 1] = _keyBuffer[i]; // 키 버퍼를 한칸씩 밀어준다.
+	}
+
+	int keyTemp = 0;
+
+	if (KEYMANAGER->isStayKeyDown(KEY_UP))		keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_DOWN))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_LEFT))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_RIGHT))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_PUNCH))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_PPUNCH))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_KICK))	keyTemp += C_UP;
+	if (KEYMANAGER->isStayKeyDown(KEY_PKICK))	keyTemp += C_UP;
+}
+
+void CommandQueue::updateCommandBuffer()
+{
+
+}
+
+void CommandQueue::renderKeyBuffer()
+{
+
+}
+
+void CommandQueue::renderCommandBuffer()
+{
+
+}
+
+bool CommandQueue::checkCommand(vector<int> vInt) // 기술 등록시 사용하는 함수
+{
+	return false;
+}
---
+++ 팀포폴용프레임워크/commandQueue.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "gameNode.h"
+#include "commandQueueDefine.h"
+#include <vector>
+
+
+
+class CommandQueue : public gameNode
+{
+private:
+	int _keyBuffer[KEYBUFFERSIZE];
+	int _commandBuffer[COMMANDBUFFERSIZE];
+
+	bool _debugMode;
+
+public:
+	CommandQueue();
+	~CommandQueue();
+
+	virtual HRESULT init(void);
+	virtual void release(void);
+	virtual void update(void);
+	virtual void render(void);
+
+	void initKeyBuffer();
+	void initCommandBuffer();
+
+	void updateKeyBuffer();
+	void updateCommandBuffer();
+
+	void renderKeyBuffer();
+	void renderCommandBuffer();
+
+	bool checkCommand(vector<int> vInt); // 기술 등록시 사용하는 함수.
+	//int형 벡터에 커맨드를 순차적으로 넣으면 됨. 커맨드는 commandQueueDefine참조
+};
