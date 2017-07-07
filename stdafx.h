b6f60ef0a93a18aa0798636b3e113b3ed417bfeb
kys9096 <kys9096@naver.com>
2017-07-07 18:19:51 +0900

    오류수정중

--- stdafx.h
+++ stdafx.h
@@ -23,14 +23,6 @@
 #include "txtData.h"
 #include "digManager.h"
 
-/////////공용헤더는 여기에 추가//////////
-
-#include "CommandQueueDefine.h"
-#include "CameraDefine.h"
-
-///////////////////////////////////////
-
-
 using namespace std;
 using namespace THETA_UTIL;
 
